214886266c84bc1785f08a1731713c3de6758cac
FergusFitzpatrick <fergusfitz@gmail.com>
2020-05-24 23:02:47 +0100

    created .csv file, trying to import but getting inconsistanet error.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, request, render_template
+import os
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("questions.csv")
+    reader = csv.reader(f)
+    for id,name,answer,fakeanswer1,fakeanswer2,fakeanswer3,category,image in reader:
+		db.execute("INSERT INTO questions (id, name, answer, fakeanswer1, fakeanswer2, fakeanswer3, category, image) VALUES (:id, :name, :answer, :fakeanswer1, :fakeanswer2, :fakeanswer3, :category, :image)",
+			{"id": id, "name": name, "answer": answer, "fakeanswer1": fakeanswer1, "fakeanswer2": fakeanswer2, "fakeanswer3": fakeanswer3, "category": category, "image": image})
 
 @app.route('/')
 def index():
